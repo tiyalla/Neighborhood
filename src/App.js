e62d7ade65182f4a54645fe048e7147d2aac1e70
tonye <tonye.iyalla@yahoo.com>
2018-12-02 12:08:51 -0500

    Initialize google map with api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap();
+  }
+  renderMap = () => {
+    loadJS("https://maps.googleapis.com/maps/api/js?key=AIzaSyBliaNHM1mAeIbnRVVwrMeZjsdIQ7iGB6c&callback=initMap")
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+   const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
   render() {
     return (
       <main>
@@ -10,6 +24,16 @@ class App extends Component {
       </main>
     );
   }
+
+ 
 }
 
 export default App;
+function loadJS(url){
+  var index = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src= url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
+}
